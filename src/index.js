0022628364fc87efefb92fd0d88323844babb260
Degera <dousan@alunos.utfpr.edu.br>
2021-10-11 20:10:36 -0300

    first_part_mobile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React ,{useEffect,useState} from 'react'
-import {View,SafeAreaView, Text, StyleSheet, StatusBar, FlatList} from 'react-native'
+import {View,SafeAreaView, Text, StyleSheet, StatusBar, FlatList,TouchableOpacity} from 'react-native'
 import api from './services/api'
 //Aestrutura de react-native nao possuem valor semantico 
 //View eh todo o tipo de secao do html como div aside section 
@@ -10,10 +10,20 @@ export default function App(){
 
     useEffect(()=>{
         api.get('projects').then(response=>{
-            console.log(response.data)
             setProjects(response.data)
         })
     }, [])
+
+    async function handleAddProject(){
+            // setProjects([...projects,`Novo Projeto ${Date.now()}`])
+              const response = await api.post('projects',{
+                title: `New Projeto ${Date.now()}`,
+                owner: 'DEGERA'
+            })
+            const newProject = response.data
+
+        setProjects = ([...projects, newProject])
+    }
 
    return (
         <>
@@ -21,14 +31,21 @@ export default function App(){
 
         <SafeAreaView style={styles.container}>
         <FlatList
-            
+            style={styles.container}
             data={projects}
-            keyExtractor={project => projects.id}
+            keyExtractor={project => project.id}
             renderItem={({item:project})=> (
-            <Text style={styles.project}key={project.id}>{project.title}</Text>
+            <Text style={styles.project}>{project.title}</Text>
             )
         }
         />
+
+        <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={handleAddProject}  >
+            <Text style={styles.butText}>  Add Project  </Text>
+        </TouchableOpacity>
         </SafeAreaView>
 
         {/*
@@ -51,5 +68,20 @@ const styles = StyleSheet.create({
         fontWeight:'bold',
         fontSize: 50,
         marginBottom: 60,
+    },
+    button:{
+        backgroundColor: '#FFF',
+        margin: 48,
+        height: 48,
+        borderRadius: 10,
+        alignItems:'center',
+        justifyContent: 'center'
+    },
+    butText: {
+        color: 'purple',
+        fontSize: 24,
+        fontFamily: 'Segoe UI',
+        fontWeight: 'bold'
     }
+    
 })
